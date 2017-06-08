6342e4f97f6762546f396b6a30156e10557b66cc
tbw <theobreuerweil@gmail.com>
2017-06-08 20:43:11 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='bnfparsing',
     packages=['bnfparsing'],
     test_suite='tests',
-    version='0.1.2',
+    version='0.1.3',
     description='A BNF parser generator for Python',
     long_description=open('README.rst').read(),
 
